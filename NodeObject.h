ed2909e08f613387a2bb5efd64d24b1b555a2030
Melon1566 <asthomas1@liberty.edu>
2018-11-27 13:21:07 -0500

    NodeObject ADT

--- NodeObject.h
+++ NodeObject.h
@@ -14,7 +14,29 @@ Node<E>* left;  //leftside pointer
 
 public:
 
+Node()//default constructor
+{
+right = NULL;
+left = NULL;
 
+}
+
+Node(E v, Node<E>* r, Node<E>* l)//constructor with value and pointers passed
+{
+right = r;
+left = l;
+value = v;
+
+}
+
+Node(E v)//constructor with just value parameter
+{
+right = NULL;
+left = NULL;
+value = v;
+
+
+}
 
 
 }
