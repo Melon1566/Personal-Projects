ba8e65e937eab60aa31f84fcd778102b13d2caa2
Melon1566 <asthomas1@liberty.edu>
2018-11-27 13:13:54 -0500

    NodeObject ADT

---
+++ NodeObject.h
@@ -0,0 +1,21 @@
+#include <iostream>
+
+
+template<typename E>
+class Node
+{
+private:
+
+E value;        //value for the node
+
+Node<E>* right; //rightside pointer
+
+Node<E>* left;  //leftside pointer
+
+public:
+
+
+
+
+}
+
